2eb06f593b0236405e02eee9ac6852b3f0ee5c6e
rayl-tanmay <findtanmay121@gmail.com>
2025-03-04 20:44:20 +0530

    Method to add Student Details

---
+++ StudentOperations.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class StudentOperations {
+ ArrayList<Student> students; // array list of data type Student (object) which is a combination of String and
+                              // int
+public StudentOperations() {
+  this.students = new ArrayList<>();
+ }
+  // Method to add students to the ArrayList
+ public void addStudents(Student student) {
+  students.add(student);
+  System.out.println("Student added.");
+ }
+}
