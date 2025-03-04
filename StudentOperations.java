3e815d1319b0e104614eb2cca22490e197b0326e
rayl-tanmay <findtanmay121@gmail.com>
2025-03-04 22:45:45 +0530

    Method to Search By Position

--- StudentOperations.java
+++ StudentOperations.java
@@ -34,4 +34,13 @@ public StudentOperations() {
   }
   return null;
  }
+  // Search Student by Position (Index)
+ public Student searchByPosition(int position) {
+  if (position >= 0 && position < students.size()) {
+   return students.get(position); // Get the student at the given index
+  } else {
+   System.out.println("Invalid position! Please enter a valid index.");
+   return null; // Return null if the position is out of bounds
+  }
+ }
 }
