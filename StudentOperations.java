4306bd8cddd4be68627282087a97fe6c298b12dd
rayl-tanmay <findtanmay121@gmail.com>
2025-03-04 22:52:43 +0530

    Delete Student By Prn

--- StudentOperations.java
+++ StudentOperations.java
@@ -57,4 +57,15 @@ public StudentOperations() {
   System.out.println("Student not found!");
   return false;
  }
+  // Delete Student by PRN
+ public boolean deleteStudent(long prn) {
+  Student student = searchByPRN(prn);
+  if (student != null) {
+   students.remove(student);
+   System.out.println("Student removed successfully.");
+   return true;
+  }
+  System.out.println("Student not found!");
+  return false;
+ }
 }
